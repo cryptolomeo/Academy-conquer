f8d9486e94bcd55d06b6274ca6942957a41ba16d
Ivan Rodriguez <taliantcode@gmail.com>
2026-09-15 10:57:46 +0200

    build(vite): configure multi-page application inputs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
-// Maneja las rutas de los enlaces convirtiendolas en relativas
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
 export default defineConfig({
-    base: "./",
-    /*
-    base: "/Wellness-Spa-Website/",     //ADD name repo
-    */
+  base: "./",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        avisoLegal: resolve(__dirname, "aviso-legal.html"),
+        blog: resolve(__dirname, "blog.html"),
+        contacto: resolve(__dirname, "contacto.html"),
+        cursos: resolve(__dirname, "cursos.html"),
+        login: resolve(__dirname, "login.html"),
+        quienesSomos: resolve(__dirname, "quienessomos.html"),
+        registro: resolve(__dirname, "registro.html"),
+        cursoBlockchain: resolve(__dirname, "cursos/curso-blockchain.html"),
+        cursoFullStack: resolve(__dirname, "cursos/curso-full-stack.html"),
+      },
+    },
+  },
 });
